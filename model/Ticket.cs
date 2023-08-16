d82ebde7ecb90ef989d1c58d1a36d8bfe941fe02
Snipes09 <125398172+Snipes09@users.noreply.github.com>
2023-08-16 13:25:45 +0200

    ticket model for ticket system

--- model/Ticket.cs
+++ model/Ticket.cs
@@ -2,18 +2,31 @@
 
 public class Ticket : IComparable<Ticket>
 {
+    public int Id { get; set; }
+    public DateTime Datum { get; set; }
+    public DateTime Verkaufsdatum { get; set; }
+    public decimal Preis { get; set; }
+    public int UserId { get; set; }
+
     public int CompareTo(Ticket? other)
     {
-        throw new NotImplementedException();
+        if (other == null) return 1;
+
+        return Datum.CompareTo(other.Datum);
     }
 
     public override bool Equals(object? obj)
     {
-        throw new NotImplementedException();
+        
+        if (obj == null || GetType() != obj.GetType()) return false;
+
+        var other = obj as Ticket;
+        return Id == other?.Id;
     }
 
     public override string ToString()
     {
-        throw new NotImplementedException();
+        
+        return $"Id: {Id}, Datum: {Datum}, Verkaufsdatum: {Verkaufsdatum}, Preis: {Preis}, UserId: {UserId}";
     }
 }
