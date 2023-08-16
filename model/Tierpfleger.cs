810d6bc89659eb52f0ad9e0bf6eb5421f30883e9
Snipes09 <125398172+Snipes09@users.noreply.github.com>
2023-08-16 13:32:40 +0200

    management methods for animals

---
+++ model/Tierpfleger.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using ZooAPI.Service;
+
+namespace ZooAPI.model;
+
+public class Tierpfleger : Mitarbeiter
+{
+    private static TierpflegerService _service;
+    public ICollection<Animal> Animals { get; set; }
+
+
+
+    public Tierpfleger()
+    {
+
+    }
+
+    
+    public async Task<ICollection<Animal>> GetAllAnimalsOfEmployeeById(int id)
+    {
+        return await _service.GetTiereByPflegerAsync(id);
+    }
+
+   
+    public async Task<int> UpdateAnimal(Animal animal)
+    {
+        return await _service.UpdateTierAsync(animal);
+    }
+    
+  
+    public async Task<Animal> GetAnimalByName(string name)
+    {
+        return await _service.GetTierByNameAsync(name);
+    }
+
+  
+
+}
